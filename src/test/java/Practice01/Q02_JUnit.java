69ae04453e32c88a9889f5e0254b07130a75015e
Ali İhsan Şen <av.aliihsan1@gmail.com>
2022-10-19 23:27:33 +0300

    19/10/2022-2

--- src/test/java/Practice01/Q02_JUnit.java
+++ src/test/java/Practice01/Q02_JUnit.java
@@ -12,6 +12,7 @@ public class Q02_JUnit {
 
     @AfterClass
     public static void afterClass() {
+
         System.out.println("After Class");
     }
 
@@ -29,16 +30,19 @@ public class Q02_JUnit {
 
     @Test
     public void test01() {
+
         System.out.println("Test 01");
     }
 
     @Test
     public void test02() {
+
         System.out.println("Test 02");
     }
 
     @Test
     public void test03() {
+
         System.out.println("Test 03");
     }
 }
